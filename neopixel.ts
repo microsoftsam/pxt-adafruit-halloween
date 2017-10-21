0c393f1ab300869853175af08baa15d1da2aea92
Sam El-Husseini <samelh@microsoft.com>
2017-10-21 13:47:16 -0700

    Updating animations

--- neopixel.ts
+++ neopixel.ts
@@ -1,7 +1,7 @@
 
 namespace light {
 
-    export class HalloweenAnimation extends light.NeoPixelAnimation {
+    export class RandomAnimation extends light.NeoPixelAnimation {
         public rgb: number;
         public delay: number;
 
@@ -40,13 +40,10 @@ namespace light {
             this.delay = delay;
 
             this.i = 0;
-            this.reveal = true;
         }
 
         public showFrame(strip: NeoPixelStrip): void {
             let l = this.rgb.length;
-            if (this.start < 0) this.start = control.millis();
-            const now = control.millis() - this.start;
             strip.setAll(light.fade(this.rgb[this.i], Math.randomRange(0, 100)))
             strip.show();
             if (this.i < l) {
@@ -59,7 +56,7 @@ namespace light {
     }
 
 
-    export class HalloweenFireAnimation extends light.NeoPixelAnimation {
+    export class FireAnimation extends light.NeoPixelAnimation {
         public rgb: number;
         public delay: number;
 
@@ -72,13 +69,10 @@ namespace light {
             this.delay = delay;
 
             this.i = 0;
-            this.reveal = true;
         }
 
         public showFrame(strip: NeoPixelStrip): void {
             const l = strip.length();
-            if (this.start < 0) this.start = control.millis();
-            const now = control.millis() - this.start;
             strip.setPixelColor(this.i, light.fade(this.rgb, 255 - this.i * 400 / l));
             strip.setPixelColor(l - this.i, light.fade(this.rgb, 255 - this.i * 400 / l));
             strip.show();
@@ -92,8 +86,35 @@ namespace light {
         }
     }
 
+    export class FadeOutAnimation extends light.NeoPixelAnimation {
+        public rgb: number;
+        public delay: number;
 
-    export class HalloweenFadeAnimation extends light.NeoPixelAnimation {
+        private i: number;
+        private reveal: boolean;
+
+        constructor(rgb: number, delay: number) {
+            super();
+            this.rgb = rgb;
+            this.delay = delay;
+
+            this.i = 0;
+        }
+
+        public showFrame(strip: NeoPixelStrip): void {
+            const l = strip.length();
+            strip.setAll(light.fade(this.rgb, 200 - this.i * 50 / l));
+            strip.show();
+            if (this.i < l * 4) {
+                this.i++;
+            } else {
+                this.i = 0;
+            }
+            loops.pause(this.delay);
+        }
+    }
+
+    export class FadeInOutAnimation extends light.NeoPixelAnimation {
         public rgb: number;
         public delay: number;
 
@@ -111,14 +132,13 @@ namespace light {
 
         public showFrame(strip: NeoPixelStrip): void {
             const l = strip.length();
-            if (this.start < 0) this.start = control.millis();
-            const now = control.millis() - this.start;
-            strip.setAll(light.fade(this.rgb, 200 - this.i * 50 / l));
+            strip.setAll(light.fade(this.rgb, 200 - (this.reveal ? this.i : l * 4 - this.i) * 50 / l));
             strip.show();
             if (this.i < l * 4) {
                 this.i++;
             } else {
                 this.i = 0;
+                this.reveal = !this.reveal;
             }
             loops.pause(this.delay);
         }
@@ -127,22 +147,26 @@ namespace light {
 
 namespace LightAnimation {
     //% blockImage=1s
-    //% block="halloween"
-    export const Halloween = new light.HalloweenAnimation(0xFF4500, 100)
+    //% block="halloween yellow"
+    export const HalloweenYellow = new light.RandomAnimation(0xFF4500, 100)
     
     //% blockImage=1s
-    //% block="spooky"
+    //% block="halloween spooky"
     export const HalloweenSpooky = new light.ColorCycleAnimation([0xFF0000, 0xFF4500], 100)
     
     //% blockImage=1s
-    //% block="fire"
-    export const HalloweenFire = new light.HalloweenFireAnimation(0xFF0000, 50)
+    //% block="halloween fire"
+    export const HalloweenFire = new light.FireAnimation(0xFF0000, 50)
     
     //% blockImage=1s
-    //% block="ghost"
-    export const HalloweenGhost = new light.HalloweenFadeAnimation(0xFFFFFF, 30)
+    //% block="halloween blue"
+    export const HalloweenBlue = new light.FadeInOutAnimation(0xFFFFFF, 30)
     
     //% blockImage=1s
-    //% block="fire fade"
-    export const HalloweenFade = new light.HalloweenFadeAnimation(0xFF4500, 5)
+    //% block="halloween green"
+    export const HalloweenGreen = new light.FadeOutAnimation(0xFF4500, 5)
+
+    //% blockImage=1s
+    //% block="halloween purple"
+    export const HalloweenPurple = new light.FadeInOutAnimation(0x330033, 10)
 }
